438f0b1b3e726311f223b4d93238e468d3b33c95
robiul1hossen <robim6225@gmail.com>
2023-05-21 23:29:40 +0600

    category data added

--- src/components/ShopByCategories/CategoryShop/CategoryShop.jsx
+++ src/components/ShopByCategories/CategoryShop/CategoryShop.jsx
@@ -6,6 +6,7 @@ const CategoryShop = ({ toy }) => {
   const { img } = toy;
   return (
     <div className="grid grid-cols-4 gap-6">
+      <p>length : {toy.subcategory}</p>
       <img src={img} alt="" />
     </div>
   );
--- src/components/ShopByCategories/ShopByCategories.jsx
+++ src/components/ShopByCategories/ShopByCategories.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import CategoryShop from "./CategoryShop/CategoryShop";
 
 const ShopByCategories = () => {
   const [toys, setToys] = useState([]);
-  console.log(toys);
-  const [activeTab, setActiveTab] = useState("monsterTruck");
+  // console.log(toys);
+  const [activeTab, setActiveTab] = useState("Monster");
   const [subcategory, setSubcategory] = useState("");
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
   };
+  useEffect(() => {
+    fetch(`https://mini-motors-server.vercel.app/filter?subcategory=${subcategory}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setToys(data);
+        // console.log(data);
+      });
+  }, [activeTab]);
 
   return (
     <div className="my-10">
@@ -18,27 +25,27 @@ const ShopByCategories = () => {
 
       <div className=" flex justify-center gap-5 items-center">
         <div
-          onClick={() => handleTabClick("monsterTruck")}
-          className={`${activeTab == "monsterTruck" ? "bg-secondary text-white font-semibold p-2" : ""}`}
+          onClick={() => handleTabClick("Monster")}
+          className={`${activeTab == "Monster" ? "bg-secondary text-white font-semibold p-2" : ""}`}
         >
           Monster Truck
         </div>
         <div
-          onClick={() => handleTabClick("policeCar")}
-          className={`${activeTab == "policeCar" ? "bg-secondary  text-white font-semibold p-2" : ""}`}
+          onClick={() => handleTabClick("Police")}
+          className={`${activeTab == "Police" ? "bg-secondary  text-white font-semibold p-2" : ""}`}
         >
           Police Car
         </div>
         <div
-          onClick={() => handleTabClick("classicCar")}
-          className={`${activeTab == "classicCar" ? "bg-secondary  text-white font-semibold p-2" : ""}`}
+          onClick={() => handleTabClick("Classic")}
+          className={`${activeTab == "Classic" ? "bg-secondary  text-white font-semibold p-2" : ""}`}
         >
           Classic Car
         </div>
-        {toys?.map((toy) => (
-          <CategoryShop toy={toy} key={toy._id}></CategoryShop>
-        ))}
       </div>
+      {toys?.map((toy) => (
+        <CategoryShop toy={toy} key={toy._id}></CategoryShop>
+      ))}
     </div>
   );
 };
