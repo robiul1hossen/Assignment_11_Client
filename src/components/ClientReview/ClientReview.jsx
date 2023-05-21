288d706d0b3416831ad47a5913c2bf2629a8db7d
robiul1hossen <robim6225@gmail.com>
2023-05-21 22:00:34 +0600

    some changes added

--- src/components/ClientReview/ClientReview.jsx
+++ src/components/ClientReview/ClientReview.jsx
@@ -15,15 +15,17 @@ const ClientReview = () => {
       </div>
       <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
-          <div className="ms-7 flex items-center w-1/2">
-            <p className="" style={{ fontSize: "24px", lineHeight: "1.5", color: "#111" }}>
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
-              maecenas accumsan lacus vel facilisis.”
-              <p className="fs-2 mt-4 ">David Adam</p>
-            </p>
+          <div className="lg:flex">
+            <div className="ms-7 flex items-center lg:w-1/2">
+              <p className="" style={{ fontSize: "24px", lineHeight: "1.5", color: "#111" }}>
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
+                maecenas accumsan lacus vel facilisis.”
+                <p className="fs-2 mt-4 ">David Adam</p>
+              </p>
+            </div>
+            <img src="https://i.ibb.co/10QWDs5/download.png" className="lg:w-1/2" />
           </div>
-          <img src="https://i.ibb.co/10QWDs5/download.png" className="w-1/2" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -34,15 +36,17 @@ const ClientReview = () => {
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <div className="ms-7 flex items-center w-1/2">
-            <p style={{ fontSize: "24px", lineHeight: "1.5", color: "#111" }}>
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin pharetra felis
-              nec rhoncus. Nam et varius magna, sit amet scelerisque mi. In sit amet eros vel risus porttitor
-              convallis. Curabitur in massa in magna posuere viverra. Sed convallis justo eu ”
-              <p className="fs-2 mt-4 ">Christopher Nolan</p>
-            </p>
+          <div className="lg:flex">
+            <div className="ms-7 flex items-center lg:w-1/2">
+              <p style={{ fontSize: "24px", lineHeight: "1.5", color: "#111" }}>
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin pharetra felis
+                nec rhoncus. Nam et varius magna, sit amet scelerisque mi. In sit amet eros vel risus
+                porttitor convallis. Curabitur in massa in magna posuere viverra. Sed convallis justo eu ”
+                <p className="fs-2 mt-4 ">Christopher Nolan</p>
+              </p>
+            </div>
+            <img src="https://i.ibb.co/THxHgRj/choose.png" alt="Second slide" className="lg:w-1/2" />
           </div>
-          <img src="https://i.ibb.co/THxHgRj/choose.png" alt="Second slide" className="w-1/2" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -53,15 +57,17 @@ const ClientReview = () => {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <div className="ms-7 flex items-center w-1/2">
-            <p style={{ fontSize: "24px", lineHeight: "1.5", color: "#111" }}>
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
-              maecenas accumsan lacus vel facilisis.”
-              <p className="fs-2 mt-4 ">David Adam</p>
-            </p>
+          <div className="lg:flex">
+            <div className="ms-7 flex items-center lg:w-1/2">
+              <p style={{ fontSize: "24px", lineHeight: "1.5", color: "#111" }}>
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
+                maecenas accumsan lacus vel facilisis.”
+                <p className="fs-2 mt-4 ">David Adam</p>
+              </p>
+            </div>
+            <img src="https://i.ibb.co/10QWDs5/download.png" className="lg:w-1/2" />
           </div>
-          <img src="https://i.ibb.co/10QWDs5/download.png" className="w-1/2" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -72,15 +78,17 @@ const ClientReview = () => {
           </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full">
-          <div className="ms-7 flex items-center w-1/2">
-            <p style={{ fontSize: "24px", lineHeight: "1.5", color: "#111" }}>
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin pharetra felis
-              nec rhoncus. Nam et varius magna, sit amet scelerisque mi. In sit amet eros vel risus porttitor
-              convallis. Curabitur in massa in magna posuere viverra. Sed convallis justo eu ”
-              <p className="fs-2 mt-4 ">Christopher Nolan</p>
-            </p>
+          <div className="lg:flex">
+            <div className="ms-7 flex items-center lg:w-1/2">
+              <p style={{ fontSize: "24px", lineHeight: "1.5", color: "#111" }}>
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin pharetra felis
+                nec rhoncus. Nam et varius magna, sit amet scelerisque mi. In sit amet eros vel risus
+                porttitor convallis. Curabitur in massa in magna posuere viverra. Sed convallis justo eu ”
+                <p className="fs-2 mt-4 ">Christopher Nolan</p>
+              </p>
+            </div>
+            <img src="https://i.ibb.co/whkZ5Pb/image6.png" className="lg:w-1/2" />
           </div>
-          <img src="https://i.ibb.co/whkZ5Pb/image6.png" className="w-1/2" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
